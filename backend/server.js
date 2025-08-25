dc8ca731c11dbdc3365562e318c1f19a76daa7eb
domimario <mariodomi@tuta.io>
2025-08-26 00:54:42 +0200

    pf

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.listen(PORT, () => {console.log(` Server is running on port ${PORT}`);});
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
