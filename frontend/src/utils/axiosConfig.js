fd7303292e291f5249b899e78e6f10d5eb6f13f2
domimario <mariodomi@tuta.io>
2025-08-25 14:46:20 +0200

    fix the changes of db

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,34 +1,36 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://aie-ar4l.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// Add token to requests
 api.interceptors.request.use(
-    (config)=>{
-        const token = localStorage.getItem("token");
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config;
-    },
-    (error)=> Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
+// Handle unauthorized responses
 api.interceptors.response.use(
-    (response)=>response,
-    (error)=>{
-        if(error.response?.status === 401){
-            console.warn("Token invalid ose skaduar");
-            localStorage.removeItem("token");
-            localStorage.removeItem("user");
-            window.location.href=("/login");
-        }
-        return Promise.reject(error)
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Token invalid ose skaduar");
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login"; // redirect user to login
     }
-)
+    return Promise.reject(error);
+  }
+);
 
 export default api;
